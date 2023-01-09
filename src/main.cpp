856b8e843635c8c2f7d329bf61b5060ba8ff850f
Mark Vincent II <41304553+MarkV2323@users.noreply.github.com>
2023-01-08 20:56:30 -0700

    Added functionality for handling user input for program launching

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "header/programOne.hpp"
 
+/* Could also do using std::cout for example */
 using namespace std;
 
 /* Print list of avaible sub programs to the user */
@@ -9,11 +11,58 @@ void printSubPrograms() {
     return;
 }
 
+/* Handle user input from the user */
+int handleUserInput() {
+    string uInp = "";
+    cin >> uInp;
+    // must be postive numeric
+    bool check = !uInp.empty() && uInp.find_first_not_of("0123456789") == std::string::npos;
+    if (!check) {
+        cout << "Non numeric characters (or negative numerics) not accepted." << endl;
+        return -1;
+    }
+    int option = std::stoi(uInp);
+    check = 0 <= option && 1 >= option;
+    if (!check) {
+        cout << "Non valid option selected." << endl;
+        return -1;
+    }
+    return option;
+}
+
 /* Entry point of program */
-int main() {
+int main(int argc, char* argv[]) {
     cout << "Welcome to the main program for Intro to C++" << endl;
     cout << "Each listed option contains a subprogram you can run, please choose one" << endl;
     printSubPrograms();
     
+    // user input vars
+    bool validChoice = false;
+    int selectedProgram = -1;
+
+    // Poll until valid choice is given
+    while (!validChoice) {
+        selectedProgram = handleUserInput();
+        if (selectedProgram != -1) {
+            validChoice = true;
+        }
+    }
+
+    /* printf allows for formatted output, some quick tips: (https://cplusplus.com/reference/cstdio/printf/)
+    General format: %[flags][width][.precision][length]specifier
+    d or i | signed decimal integer
+    s | string
+    c | character 
+    */
+    switch (selectedProgram) {
+    case 1:
+        printf("Launching program %d...\n", selectedProgram);
+        programOneEntry();
+        break;
+    default:
+        break;
+    }
+
+    printf("Exiting program...\n");
     return 0;
 }
