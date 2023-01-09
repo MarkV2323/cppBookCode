0ea596cf4e5c54f8df2c70c264f985d1643ccefd
Mark <41304553+MarkV2323@users.noreply.github.com>
2023-01-08 20:01:23 -0700

    Variblized compiliation files and flags

--- build.sh
+++ build.sh
@@ -1,4 +1,25 @@
+# Varible containing all compiler flags
+B_FLAGS="\
+-std=c++17 \
+-Wall \
+-o main.exe \
+-I ./
+"
+
+# Varible containing all build files
+B_FILES="\
+src/main.cpp \
+src/programOne.cpp
+"
+
+# Remove any artifacts from previous
 rm -f main.exe
-g++ -o main.exe main.cpp -std=c++17
+
+# Begin compiling
+g++ $B_FLAGS $B_FILES
+
+# run binary
 ./main.exe
+
+# cleanup
 rm -f main.exe
