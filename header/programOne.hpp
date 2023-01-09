da4691013ec151aec7a4161f078b1c76045e029b
Mark Vincent II <41304553+MarkV2323@users.noreply.github.com>
2023-01-08 20:56:51 -0700

    Completed gatherUserInput example

--- header/programOne.hpp
+++ header/programOne.hpp
@@ -1,6 +1,16 @@
 /* Include Gaurd only allows this file to be compiled once */
 #pragma once
+#include <iostream>
+#include <string>
 
+using std::getline;
+using std::string;
+using std::endl;
+using std::cout;
+using std::cin;
 
 /* program functions */
-bool gatherInput();
+void programOneEntry();
+void gatherUserInput();
+void printPrimTypeInfo();
+string cStringExample();
--- src/programOne.cpp
+++ src/programOne.cpp
@@ -1,1 +1,38 @@
 #include "header/programOne.hpp"
+
+/* Example: get input from the user */
+void gatherUserInput() {
+    // local varible (only access from gatherUserInput)
+    string userInput = "";
+    /* can use cin for raw (can't get entire line), getLine for refined gathering (of entire line) */
+    cout << "Please enter some string" << endl;
+    cin >> userInput;
+    cout << "What you entered: " << userInput << endl;
+    // flush cin (reccomended to do if using cin)
+    cin.ignore (100,'\n');
+    // clear input
+    userInput = "";
+    cout << "Please enter some string" << endl;
+    getline(cin, userInput);
+    cout << "What you entered: " << userInput << endl;
+    // clear input
+    userInput = "";
+    return;
+}
+
+/* Example: print out primitive type information */
+void printPrimTypeInfo() {
+    return;
+}
+
+/* Example: showcase differences between C-Strings (char array) & strings */
+string cStringExample() {
+    return "";
+}
+
+/* Entry point of program one */
+void programOneEntry() {
+    // call user input example function
+    gatherUserInput();
+    return;
+}
